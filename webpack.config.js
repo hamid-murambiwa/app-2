b9f9f94025e9cfa4935b29d0664bac776790ed28
hamid-murambiwa <machipisajunior@gmail.com>
2021-10-15 09:48:37 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: "/webpack-demo/",
+    publicPath: "/app-2/",
   },
   module: {
     rules: [
